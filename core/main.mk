6803944b97ccfdfa0cbf931745f4549e82ede100
Paul Kocialkowski <contact@paulk.fr>
2013-10-28 17:49:31 +0100

    Don't check for the non-free Java version
    Change-Id: I1a7069ee9902d13b1615263df7be68cf02aa78e9
    Signed-off-by: Paul Kocialkowski <contact@paulk.fr>

--- core/main.mk
+++ core/main.mk
@@ -120,25 +120,6 @@ $(warning ************************************************************)
 $(error Directory names containing spaces not supported)
 endif
 
-
-# Check for the correct version of java
-java_version := $(shell java -version 2>&1 | head -n 1 | grep '^java .*[ "]1\.[67][\. "$$]')
-ifneq ($(shell java -version 2>&1 | grep -i openjdk),)
-java_version :=
-endif
-ifeq ($(strip $(java_version)),)
-$(info ************************************************************)
-$(info You are attempting to build with an unsupported version)
-$(info of java.)
-$(info $(space))
-$(info Your version is: $(shell java -version 2>&1 | head -n 1).)
-$(info The correct version is: Java SE 1.6 or 1.7.)
-$(info $(space))
-$(info Please follow the machine setup instructions at)
-$(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
-$(info ************************************************************)
-endif
-
 # Check for the correct version of javac
 javac_version := $(shell javac -version 2>&1 | head -n 1 | grep '[ "]1\.[67][\. "$$]')
 ifeq ($(strip $(javac_version)),)
@@ -148,9 +129,6 @@ $(info of javac.)
 $(info $(space))
 $(info Your version is: $(shell javac -version 2>&1 | head -n 1).)
 $(info The correct version is: 1.6 or 1.7.)
-$(info $(space))
-$(info Please follow the machine setup instructions at)
-$(info $(space)$(space)$(space)$(space)https://source.android.com/source/download.html)
 $(info ************************************************************)
 $(error stop)
 endif
